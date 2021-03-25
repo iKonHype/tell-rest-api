f02b5aa455544638edb889ae0bc6ee48c0f3678f
kaveenhyacinth <alwiskaveen@gmail.com>
2021-03-25 07:10:55 +0530

    Addd enum helper

---
+++ helpers/Enumerations.js
@@ -0,0 +1,12 @@
+/**
+ * @module helpers/Enumeration
+ * @enum {string} status
+ */
+exports.status = {
+  open: "open",
+  accepted: "accepted",
+  rejected: "rejected",
+  processing: "processing",
+  done: "done",
+  confirmed: "confirmed",
+};
