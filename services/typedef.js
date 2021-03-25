6f50d25e47981e2f69583e815063b53b452e5ddc
kaveenhyacinth <alwiskaveen@gmail.com>
2021-03-25 07:11:34 +0530

    Add typedef for jsDoc

--- services/typedef.js
+++ services/typedef.js
@@ -4,3 +4,22 @@
  * @property {object|string|object[]|string[]} result promise results
  * @property {boolean} success true|false
  */
+const defaultReturnType = {
+  result: null,
+  success: null,
+};
+
+/**
+ * @description Default location structure
+ * @typedef {Object} Location
+ * @property {string} line Street address
+ * @property {string} city Hometown
+ * @property {string} postal Area postal code
+ * @property {string} district Home district
+ */
+const Location = {
+  line: null,
+  city: null,
+  postal: null,
+  district: null,
+};
