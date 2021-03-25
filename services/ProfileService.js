425073554adc8cb9b37843ec24a995d572315917
kaveenhyacinth <alwiskaveen@gmail.com>
2021-03-25 13:14:31 +0530

    Minor fix

--- services/ProfileService.js
+++ services/ProfileService.js
@@ -1,59 +1,56 @@
+/**
+ * @module service/profile
+ * @require module:model/user
+ */
 const User = require("../models/User");
 
+/**
+ * Update user profile
+ * @async
+ * @param {object} payload HTTP request body
+ * @returns {defaultReturnType} Updated profile
+ */
+exports.updateProfile = async (payload) => {
+  try {
+    const userId = payload.userId;
+    payload.userId = undefined;
 
+    const result = await User.findByIdAndUpdate(
+      userId,
+      {
+        $set: payload,
+      },
+      { new: true }
+    );
+    if (!result) return { result, success: false };
 
-//update profile
-const updateProfile =async (payload) => {
-try {
-  const userId=payload.userId;
-  payload.userId = undefined;
-  
-  const result = await User.findByIdAndUpdate(userId,{$set:payload},{new:true});
-
-  if(!result)return{result,success:false};
-  return{result,success:true};
-} catch (error) {
-  return {result:error.message,success:false}
-}
-
-
+    return { result, success: true };
+  } catch (error) {
+    return { result: error.message, success: false };
+  }
 };
 
-//dispaly/read profile 
-const readProfile = (payload)=>{
-  
-try {
-  const userId = payload.userId;
-  payload.userId = undefined;
+//dispaly/read profile
+exports.readProfile = async (userId) => {
+  try {
+    const result = await User.findById(userId);
+    if (!result) return { result, success: false };
 
-  const result = await User.findOne(userId,{$set:payload},{new:true});
-
-  if(!result)return{result,success:false};
-  return{result,success:true};
-
-} catch (error) {
-  return{result:error.message,success:false}
-}
+    return { result, success: true };
+  } catch (error) {
+    return { result: error.message, success: false };
+  }
   //const result=await User.
 };
 
 //delete profile
 
-const deleteProfile=(userId)=>{
- 
+exports.deleteProfile = async (userId) => {
   try {
-    const userId=await User.findByIdAndDelete(userId); 
+    const result = await User.findByIdAndDelete(userId);
 
-   return {result:null,success:true}
-
+    return { result, success: true };
   } catch (error) {
-    return{result:error.message,success:false}
+    return { result: error.message, success: false };
   }
-
-};
-
-module.exports = {
-  updateProfile,
-  readProfile,
-  deleteProfile
 };
