541b9fc1579ee9673e924b23923d2b45d6bcbb2e
kaveenhyacinth <alwiskaveen@gmail.com>
2021-03-25 07:11:17 +0530

    Add features to complaint service

--- services/ComplaintService.js
+++ services/ComplaintService.js
@@ -1,18 +1,52 @@
 /**
  * @module service/complaint
  * @requires module:model/complaint
+ * @requires module:helpers/Enumeration
  */
 
 const Complaint = require("../models/Complaint");
+const {status: enumStatus} = require("../helpers/Enumerations")
 
 /**
  * Create a new complaint [User]
  * @async
- * @param {Object} payload
+ * @param {objectId} userId Complainer: owner
+ * @param {string} content
+ * @param {objectId} category
+ * @param {Location} location
+ * @param {string} landmark
+ * @param {string} media
  * @returns {defaultReturnType}
  */
-exports.createNewComplaint = async (payload) => {
+exports.createNewComplaint = async (
+  userId,
+  content,
+  category,
+  location,
+  landmark,
+  media
+) => {
   try {
+    const complaint = new Complaint({
+      owner: userId,
+      content,
+      category,
+      location,
+      landmark,
+      media,
+    });
+
+    const result = await complaint.save();
+    if (!result) return { result: null, success: false };
+
+    const saveComplaintsInUser = await User.findByIdAndUpdate(
+      userId,
+      { $push: { complaints: result._id } },
+      { new: true }
+    );
+    if (!saveComplaintsInUser) return { result: null, success: false };
+
+    return { result, success: true };
   } catch (error) {
     return { result: error.message, success: false };
   }
@@ -21,14 +55,86 @@ exports.createNewComplaint = async (payload) => {
 /**
  * Update complaint status [Admin|Authority]
  * @async
- * @param {ObjectId} complaintId
- * @param {String} complaintState
+ * @param {objectId} userId Admin or Authority
+ * @param {objectId} complaintId
+ * @param {string} complaintState ['open'|'accepted'|'rejected'|'processing'|'done'|'confirmed']
  * @returns {defaultReturnType}
  */
-exports.updateComplaintStatus = async (complaintId, complaintState) => {
+exports.updateComplaintStatus = async (userId, complaintId, complaintState) => {
   try {
+    const result = await Complaint.findOneAndUpdate(
+      { authority: userId, _id: complaintId },
+      {
+        $set: { status: complaintState },
+      },
+      { new: true }
+    );
+    if (!result) return { result, success: false };
+    return { result, success: true };
   } catch (error) {
     return { result: error.message, success: false };
+  }
+};
+
+/**
+ * Comment on a complaint for suggestion and more info
+ * @async
+ * @param {objectId} userId Complainer: any
+ * @param {objectId} complaintId
+ * @param {string} content
+ * @returns {defaultReturnType}
+ */
+exports.commentOnComplaint = async (userId, complaintId, content) => {
+  try {
+    const comments = { commentor: userId, content };
+
+    const result = await Complaint.findByIdAndUpdate(
+      complaintId,
+      { $push: { comments } },
+      { new: true }
+    );
+    if (!result) return { result, success: false };
+    return { result, success: true };
+  } catch (error) {
+    return { result: error, success: false };
+  }
+};
+
+/**
+ * Upvote a complaint to change priority [User]
+ * @async
+ * @param {objectId} userId Complainer: any
+ * @param {objectId} complaintId
+ * @returns {defaultReturnType}
+ */
+exports.upvoteComplaint = async (userId, complaintId) => {
+  try {
+    // check if user upvoted before
+    const complaint = await Complaint.findOne({
+      _id: complaintId,
+      votes: userId,
+    });
+    if (complaint) {
+      // if upvoted pull user
+      const removeVote = await Complaint.findByIdAndUpdate(
+        complaintId,
+        { $pull: { votes: userId } },
+        { new: true }
+      );
+      if (!removeVote) return { result: removeVote, success: false };
+      return { result: removeVote, success: true };
+    } else {
+      // if not upvoted push user
+      const addVote = await Complaint.findByIdAndUpdate(
+        complaintId,
+        { $push: { votes: userId } },
+        { new: true }
+      );
+      if (!addVote) return { result: addVote, success: false };
+      return { result: addVote, success: true };
+    }
+  } catch (error) {
+    return { result: error, success: false };
   }
 };
 
@@ -36,11 +142,20 @@ exports.updateComplaintStatus = async (complaintId, complaintState) => {
  * Confirm when a complaint processing is done [User]
  * @async
  * @param {ObjectId} complaintId
- * @param {ObjectId} complainerId
+ * @param {ObjectId} userId Complainer: owner
  * @returns {defaultReturnType}
  */
-exports.confirmProgressDone = async (complaintId, complainerId) => {
+exports.confirmProgressDone = async (userId, complaintId) => {
   try {
+    const result = await Complaint.findOneAndUpdate(
+      { owner: userId, _id: complaintId },
+      {
+        $set: { status: "confirmed" },
+      },
+      { new: true }
+    );
+    if (!result) return { result, success: false };
+    return { result, success: true };
   } catch (error) {
     return { result: error.message, success: false };
   }
@@ -54,6 +169,18 @@ exports.confirmProgressDone = async (complaintId, complainerId) => {
  */
 exports.getComplaintById = async (complaintId) => {
   try {
+    const result = await Complaint.findById(complaintId)
+      .populate({
+        path: "owner",
+        select: "firstName lastName profImg",
+      })
+      .populate({
+        path: "comments.commentor",
+        select: "firstName lastName profImg",
+      })
+      .populate("category");
+    if (!result) return { result, success: false };
+    return { result, success: true };
   } catch (error) {
     return { result: error.message, success: false };
   }
@@ -62,11 +189,23 @@ exports.getComplaintById = async (complaintId) => {
 /**
  * Get all complaints by its category [Admin|Authority]
  * @async
- * @param {String} complaintCategory
+ * @param {string} complaintCategory
  * @returns {defaultReturnType}
  */
 exports.getAllComplaintsByCategory = async (complaintCategory) => {
   try {
+    const result = await Complaint.find({category: complaintCategory})
+      .populate({
+        path: "owner",
+        select: "firstName lastName profImg",
+      })
+      .populate({
+        path: "comments.commentor",
+        select: "firstName lastName profImg",
+      })
+      .populate("category");
+    if (!result) return { result, success: false };
+    return { result, success: true };
   } catch (error) {
     return { result: error.message, success: false };
   }
