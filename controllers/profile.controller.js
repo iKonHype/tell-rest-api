7d8ef9a07b76123ad21855be8dfe42630e89c220
yashen97 <yashenraveesha97@gmail.com>
2021-03-26 19:10:05 +0530

    R.Samarasingha C

---
+++ controllers/profile.controller.js
@@ -0,0 +1,20 @@
+const {
+   updateProfile,
+   readProfile,
+   deleteProfile,
+  } = require("../services/ProfileService");
+
+ 
+  
+//Profile update
+  const updateController=(req,res)=>{};
+//Profile read
+const readController=()=>{};
+//Profile delete
+const deleteController=()=>{};
+
+module.exports={
+  updateController,
+  readController,
+  deleteController
+};
---
+++ routes/profile.route.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+
+const {}=require("../controllers/profile.controller");
+
+//ill finish this in saturday  ;)
+
+module.exports=router;
+
