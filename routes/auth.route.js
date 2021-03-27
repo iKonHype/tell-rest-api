df40aaeb73cffbb618f3ae705dc227347053e0f9
kaveenhyacinth <alwiskaveen@gmail.com>
2021-03-27 06:42:24 +0530

    Add examples to jsdoc

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -3,7 +3,6 @@
  * @requires module:controller/auth
  * @requires module:middleware/checkpoint
  */
-
 const router = require("express").Router();
 const auth = require("../controllers/auth.controller");
 const checkpoint = require("../middlewares/checkpoint");
@@ -11,28 +10,29 @@ const checkpoint = require("../middlewares/checkpoint");
 /**
  * Sign-up user
  * @name post/signup
+ * @example {base_url}/auth/signup
  */
 router.post("/signup", checkpoint.checkEmail, auth.signupController);
 
 /**
  * activate and register user
  * @name post/activate
+ * @example {base_url}/auth/activate
  */
 router.post("/activate", checkpoint.checkEmail, auth.activateAccountController);
 
 /**
  * Sign-in user
  * @name post/signin
+ * @example {base_url}/auth/signin
  */
 router.post("/signin", auth.signinController);
 
 /**
  * refresh token
- * @name post/refresh-token
+ * @name post/refresh
+ * @example {base_url}/auth/refresh
  */
-router.post("/refresh-token", auth.refreshTokenController);
-
-// FIXME: Signout route
-router.get("/signout", auth.signoutController);
+router.post("/refresh", auth.refreshTokenController);
 
 module.exports = router;
