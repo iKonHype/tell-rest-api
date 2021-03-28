3017117bbd59ae412ea14db08fef44fa3824c39d
kaveenhyacinth <alwiskaveen@gmail.com>
2021-03-28 05:51:19 +0530

    Add authority profile functions to profile service

--- src/services/ProfileService.js
+++ src/services/ProfileService.js
@@ -1,12 +1,13 @@
 /**
  * @module service/profile
  * @requires module:model/user
- * @requires module:model.authority
+ * @requires module:model/authority
  */
 const User = require("../models/User");
 const Authority = require("../models/Authority");
 
 //#region  User Profile
+
 /**
  * Update user profile
  * @async
@@ -42,7 +43,6 @@ exports.readUserProfile = async (userId) => {
   try {
     const result = await User.findById(userId).select("-salt -encry_password");
     if (!result) return { result, success: false };
-
     return { result, success: true };
   } catch (error) {
     return { result: error.message, success: false };
@@ -66,4 +66,61 @@ exports.deleteUserProfile = async (userId) => {
 
 //#region  Authority profile
 
+/**
+ * Get authority profile by id [Admin|Authority]
+ * @async
+ * @param {objectId} authorityId
+ * @returns {defaultReturnType}
+ */
+exports.readAuthorityProfile = async (authorityId) => {
+  try {
+    const result = await Authority.findById(authorityId).select(
+      "-salt -encry_password"
+    );
+    if (!result) return { result, success: false };
+    return { result, success: true };
+  } catch (error) {
+    return { result: error.message, success: false };
+  }
+};
+
+/**
+ * Update authority profile [Admin]
+ * @async
+ * @param {object} payload
+ * @returns
+ */
+exports.updateAuthorityProfile = async (payload) => {
+  try {
+    const userId = payload.userId;
+    payload.userId = undefined;
+
+    const result = await Authority.findByIdAndUpdate(
+      userId,
+      {
+        $set: payload,
+      },
+      { new: true }
+    );
+    if (!result) return { result, success: false };
+    return { result, success: true };
+  } catch (error) {
+    return { result: error.message, success: false };
+  }
+};
+
+/**
+ * Delete authority profile [Admin]
+ * @async
+ * @param {objectId} authorityId
+ * @returns {defaultReturnType}
+ */
+exports.deleteAuthorityProfile = async (authorityId) => {
+  try {
+    const result = await Authority.findByIdAndDelete(authorityId);
+    return { result, success: true };
+  } catch (error) {
+    return { result: error.message, success: false };
+  }
+};
 //#endregion
